0585ccbb3210168065cf4979a3568fe123aa6338
Tangjj1996 <awesometangji@gmail.com>
2024-09-29 22:33:58 +0800

    feat: 编辑列表也

---
+++ src/pages/TableList/config.tsx
@@ -0,0 +1,35 @@
+import { ProColumns } from '@ant-design/pro-components';
+
+export const columns: ProColumns<any>[] = [
+  {
+    title: '标题',
+    dataIndex: 'title',
+  },
+  {
+    title: '内容',
+    dataIndex: 'content',
+  },
+  {
+    title: '标签',
+    dataIndex: 'tags',
+  },
+  {
+    title: '封面',
+    dataIndex: 'cover'
+  },
+  {
+    title: '图片集',
+    dataIndex: 'swiper'
+  },
+  {
+    title: '操作',
+    valueType: 'option',
+    width: 200,
+    render(dom, entity, index, action) {
+     return [
+      <a key='editable' onClick={() => action?.startEditable?.(entity.id)}>编辑</a>,
+      <a key='delete'>删除</a>
+     ]
+    },
+  }
+]
